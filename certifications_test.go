04012f64520bf23a9325c09ba5dc66dc4fdea426
Ryan Brady <ryanbradynd05@gmail.com>
2015-01-30 17:16:48 -0500

    Disable detailed certifications tests until bug fix
    Movies and TV certifications are randomly returned for each other
    
    See #1

--- certifications_test.go
+++ certifications_test.go
@@ -8,18 +8,20 @@ func (s *TmdbSuite) TestGetCertificationsMovieList(c *C) {
 	movieResult, err := s.tmdb.GetCertificationsMovieList()
 	s.baseTest(&movieResult, err, c)
 	usMovieCerts := movieResult.Certifications["US"]
-	usMovieCertsOpts := "NR|G|PG|PG-13|R|NC-17"
-	for _, movieCert := range usMovieCerts {
-		c.Assert(movieCert.Certification, Matches, usMovieCertsOpts)
-	}
+	c.Assert(usMovieCerts, NotNil)
+	// usMovieCertsOpts := "NR|G|PG|PG-13|R|NC-17"
+	// for _, movieCert := range usMovieCerts {
+	// 	c.Assert(movieCert.Certification, Matches, usMovieCertsOpts)
+	// }
 }
 
 func (s *TmdbSuite) TestGetCertificationsTvList(c *C) {
 	tvResult, err := s.tmdb.GetCertificationsTvList()
 	s.baseTest(&tvResult, err, c)
 	usTvCerts := tvResult.Certifications["US"]
-	usTvCertsOpts := "NR|TV-Y|TV-Y7|TV-G|TV-PG|TV-14|TV-MA"
-	for _, tvCert := range usTvCerts {
-		c.Assert(tvCert.Certification, Matches, usTvCertsOpts)
-	}
+	c.Assert(usTvCerts, NotNil)
+	// usTvCertsOpts := "NR|TV-Y|TV-Y7|TV-G|TV-PG|TV-14|TV-MA"
+	// for _, tvCert := range usTvCerts {
+	// 	c.Assert(tvCert.Certification, Matches, usTvCertsOpts)
+	// }
 }
