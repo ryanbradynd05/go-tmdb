a91b0f0a2843099eaf8f8d56fb7218ceab082a70
Ryan Brady <ryanbradynd05@gmail.com>
2015-01-31 23:20:39 -0500

    Add options to changes API

--- changes.go
+++ changes.go
@@ -12,29 +12,37 @@ type Changes struct {
 	}
 }
 
+var changeOptions = map[string]struct{}{
+	"page":       {},
+	"start_date": {},
+	"end_date":   {}}
+
 // GetChangesMovie gets a list of movie ids that have been edited
 // http://docs.themoviedb.apiary.io/#reference/changes/moviechanges/get
-func (tmdb *TMDb) GetChangesMovie() (*Changes, error) {
+func (tmdb *TMDb) GetChangesMovie(options map[string]string) (*Changes, error) {
 	var movieChanges Changes
-	uri := fmt.Sprintf("%s/movie/changes?api_key=%s", baseURL, tmdb.apiKey)
+	optionsString := getOptionsString(options, changeOptions)
+	uri := fmt.Sprintf("%s/movie/changes?api_key=%s%s", baseURL, tmdb.apiKey, optionsString)
 	result, err := callTmdb(uri, &movieChanges)
 	return result.(*Changes), err
 }
 
 // GetChangesPerson gets a list of people ids that have been edited
 // http://docs.themoviedb.apiary.io/#reference/changes/personchanges/get
-func (tmdb *TMDb) GetChangesPerson() (*Changes, error) {
+func (tmdb *TMDb) GetChangesPerson(options map[string]string) (*Changes, error) {
 	var personChanges Changes
-	uri := fmt.Sprintf("%s/person/changes?api_key=%s", baseURL, tmdb.apiKey)
+	optionsString := getOptionsString(options, changeOptions)
+	uri := fmt.Sprintf("%s/person/changes?api_key=%s%s", baseURL, tmdb.apiKey, optionsString)
 	result, err := callTmdb(uri, &personChanges)
 	return result.(*Changes), err
 }
 
 // GetChangesTv gets a list of tv show ids that have been edited
 // http://docs.themoviedb.apiary.io/#reference/changes/tvchanges/get
-func (tmdb *TMDb) GetChangesTv() (*Changes, error) {
+func (tmdb *TMDb) GetChangesTv(options map[string]string) (*Changes, error) {
 	var tvChanges Changes
-	uri := fmt.Sprintf("%s/tv/changes?api_key=%s", baseURL, tmdb.apiKey)
+	optionsString := getOptionsString(options, changeOptions)
+	uri := fmt.Sprintf("%s/tv/changes?api_key=%s%s", baseURL, tmdb.apiKey, optionsString)
 	result, err := callTmdb(uri, &tvChanges)
 	return result.(*Changes), err
 }
--- changes_test.go
+++ changes_test.go
@@ -5,21 +5,21 @@ import (
 )
 
 func (s *TmdbSuite) TestGetChangesMovie(c *C) {
-	movieResult, err := s.tmdb.GetChangesMovie()
+	movieResult, err := s.tmdb.GetChangesMovie(nil)
 	s.baseTest(&movieResult, err, c)
 	c.Assert(movieResult.Results, NotNil)
 	c.Assert(movieResult.Results, Not(HasLen), 0)
 }
 
 func (s *TmdbSuite) TestGetChangesPerson(c *C) {
-	personResult, err := s.tmdb.GetChangesPerson()
+	personResult, err := s.tmdb.GetChangesPerson(nil)
 	s.baseTest(&personResult, err, c)
 	c.Assert(personResult.Results, NotNil)
 	c.Assert(personResult.Results, Not(HasLen), 0)
 }
 
 func (s *TmdbSuite) TestGetChangesTv(c *C) {
-	tvResult, err := s.tmdb.GetChangesTv()
+	tvResult, err := s.tmdb.GetChangesTv(nil)
 	s.baseTest(&tvResult, err, c)
 	c.Assert(tvResult.Results, NotNil)
 	c.Assert(tvResult.Results, Not(HasLen), 0)
