47a8b7044d03fede89d1cfdb4b384d3783f5c62b
Ryan Brady <ryanbradynd05@gmail.com>
2015-02-01 23:52:14 -0500

    Fix golint issues

--- keywords_test.go
+++ keywords_test.go
@@ -4,28 +4,28 @@ import (
 	. "gopkg.in/check.v1"
 )
 
-const fightKeywordId int = 1721
+const fightKeywordID int = 1721
 
 func (s *TmdbSuite) TestGetKeywordInfo(c *C) {
-	result, err := s.tmdb.GetKeywordInfo(fightKeywordId)
+	result, err := s.tmdb.GetKeywordInfo(fightKeywordID)
 	s.baseTest(&result, err, c)
-	c.Assert(result.ID, Equals, fightKeywordId)
+	c.Assert(result.ID, Equals, fightKeywordID)
 	c.Assert(result.Name, Equals, "fight")
 }
 
 func (s *TmdbSuite) TestGetKeywordMovies(c *C) {
-	result, err := s.tmdb.GetKeywordMovies(fightKeywordId, nil)
+	result, err := s.tmdb.GetKeywordMovies(fightKeywordID, nil)
 	s.baseTest(&result, err, c)
-	c.Assert(result.ID, Equals, fightKeywordId)
+	c.Assert(result.ID, Equals, fightKeywordID)
 	c.Assert(result.Page, Equals, 1)
 	c.Assert(result.TotalPages > 7, Equals, true)
 	c.Assert(result.TotalResults > 150, Equals, true)
 
 	var page2Options = make(map[string]string)
 	page2Options["page"] = "2"
-	page2Result, err := s.tmdb.GetKeywordMovies(fightKeywordId, page2Options)
+	page2Result, err := s.tmdb.GetKeywordMovies(fightKeywordID, page2Options)
 	s.baseTest(&page2Result, err, c)
-	c.Assert(page2Result.ID, Equals, fightKeywordId)
+	c.Assert(page2Result.ID, Equals, fightKeywordID)
 	c.Assert(page2Result.Page, Equals, 2)
 	c.Assert(page2Result.TotalPages, Equals, result.TotalPages)
 	c.Assert(page2Result.TotalResults, Equals, result.TotalResults)
--- lists.go
+++ lists.go
@@ -40,9 +40,9 @@ func (tmdb *TMDb) GetListInfo(id string) (*ListInfo, error) {
 
 // GetListItemStatus checks to see if a movie ID is already added to a list
 // http://docs.themoviedb.apiary.io/#reference/lists/listiditemstatus/get
-func (tmdb *TMDb) GetListItemStatus(id string, movieId int) (*ListItemStatus, error) {
+func (tmdb *TMDb) GetListItemStatus(id string, movieID int) (*ListItemStatus, error) {
 	var itemStatus ListItemStatus
-	uri := fmt.Sprintf("%s/list/%v/item_status?api_key=%s&movie_id=%v", baseURL, id, tmdb.apiKey, movieId)
+	uri := fmt.Sprintf("%s/list/%v/item_status?api_key=%s&movie_id=%v", baseURL, id, tmdb.apiKey, movieID)
 	result, err := callTmdb(uri, &itemStatus)
 	return result.(*ListItemStatus), err
 }
