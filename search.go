eeef56b26a28894ab0998041f8c08ba71de5756d
Ryan Brady <ryanbradynd05@gmail.com>
2015-02-02 02:54:27 -0500

    Finish all search functions

--- search.go
+++ search.go
@@ -5,12 +5,185 @@ import (
 	"net/url"
 )
 
+// CollectionSearchResults struct
+type CollectionSearchResults struct {
+	Page    int
+	Results []struct {
+		ID           int
+		BackdropPath string `json:"backdrop_path"`
+		Name         string
+		PosterPath   string `json:"poster_path"`
+	}
+	TotalPages   int `json:"total_pages"`
+	TotalResults int `json:"total_results"`
+}
+
+// CompanySearchResults struct
+type CompanySearchResults struct {
+	Page    int
+	Results []struct {
+		ID       int
+		LogoPath string `json:"logo_path"`
+		Name     string
+	}
+	TotalPages   int `json:"total_pages"`
+	TotalResults int `json:"total_results"`
+}
+
+// KeywordSearchResults struct
+type KeywordSearchResults struct {
+	Page    int
+	Results []struct {
+		ID   int
+		Name string
+	}
+	TotalPages   int `json:"total_pages"`
+	TotalResults int `json:"total_results"`
+}
+
+// ListSearchResults struct
+type ListSearchResults struct {
+	Page    int
+	Results []struct {
+		Description   string
+		FavoriteCount int `json:"favorite_count"`
+		ID            string
+		ItemCount     int    `json:"item_count"`
+		Iso639_1      string `json:"iso_639_1"`
+		ListType      string `json:"list_type"`
+		Name          string
+		PosterPath    string `json:"poster_path"`
+	}
+	TotalPages   int `json:"total_pages"`
+	TotalResults int `json:"total_results"`
+}
+
 // MovieSearchResults struct
 type MovieSearchResults struct {
 	Page         int
 	Results      []MovieShort
 	TotalPages   int `json:"total_pages"`
 	TotalResults int `json:"total_results"`
+}
+
+// MultiSearchResults struct
+type MultiSearchResults struct {
+	Page    int
+	Results []struct {
+		BackdropPath  string `json:"backdrop_path"`
+		ID            int
+		OriginalName  string   `json:"original_name"`
+		FirstAirDate  string   `json:"first_air_date"`
+		OriginCountry []string `json:"origin_country"`
+		PosterPath    string   `json:"poster_path"`
+		Popularity    float32
+		Name          string
+		VoteAverage   float32 `json:"vote_average"`
+		VoteCount     uint32  `json:"vote_count"`
+		MediaType     string  `json:"media_type"`
+	}
+	TotalPages   int `json:"total_pages"`
+	TotalResults int `json:"total_results"`
+}
+
+// PersonSearchResults struct
+type PersonSearchResults struct {
+	Page    int
+	Results []struct {
+		Adult      bool
+		ID         int
+		Name       string
+		Popularity float32
+		PosterPath string `json:"poster_path"`
+		KnownFor   []struct {
+			Adult         bool
+			BackdropPath  string `json:"backdrop_path"`
+			ID            int
+			OriginalTitle string `json:"original_title"`
+			ReleaseDate   string `json:"release_date"`
+			PosterPath    string `json:"poster_path"`
+			Popularity    float32
+			Title         string
+			VoteAverage   float32 `json:"vote_average"`
+			VoteCount     uint32  `json:"vote_count"`
+			MediaType     string  `json:"media_type"`
+		} `json:"known_for"`
+	}
+	TotalPages   int `json:"total_pages"`
+	TotalResults int `json:"total_results"`
+}
+
+// TvSearchResults struct
+type TvSearchResults struct {
+	Page    int
+	Results []struct {
+		BackdropPath  string `json:"backdrop_path"`
+		ID            int
+		OriginalName  string   `json:"original_name"`
+		FirstAirDate  string   `json:"first_air_date"`
+		OriginCountry []string `json:"origin_country"`
+		PosterPath    string   `json:"poster_path"`
+		Popularity    float32
+		Name          string
+		VoteAverage   float32 `json:"vote_average"`
+		VoteCount     uint32  `json:"vote_count"`
+	}
+	TotalPages   int `json:"total_pages"`
+	TotalResults int `json:"total_results"`
+}
+
+// SearchCollection searches for collections by name
+// http://docs.themoviedb.apiary.io/#reference/search/searchcollection/get
+func (tmdb *TMDb) SearchCollection(name string, options map[string]string) (*CollectionSearchResults, error) {
+	var availableOptions = map[string]struct{}{
+		"page":     {},
+		"language": {}}
+	var collections CollectionSearchResults
+	safeName := url.QueryEscape(name)
+	optionsString := getOptionsString(options, availableOptions)
+	uri := fmt.Sprintf("%s/search/collection?query=%s&api_key=%s%s", baseURL, safeName, tmdb.apiKey, optionsString)
+	result, err := callTmdb(uri, &collections)
+	return result.(*CollectionSearchResults), err
+}
+
+// SearchCompany searches for companies by name
+// http://docs.themoviedb.apiary.io/#reference/search/searchcompany/get
+func (tmdb *TMDb) SearchCompany(name string, options map[string]string) (*CompanySearchResults, error) {
+	var availableOptions = map[string]struct{}{
+		"page": {}}
+	var companies CompanySearchResults
+	safeName := url.QueryEscape(name)
+	optionsString := getOptionsString(options, availableOptions)
+	uri := fmt.Sprintf("%s/search/company?query=%s&api_key=%s%s", baseURL, safeName, tmdb.apiKey, optionsString)
+	result, err := callTmdb(uri, &companies)
+	return result.(*CompanySearchResults), err
+}
+
+// SearchKeyword searches for keywords by name
+// http://docs.themoviedb.apiary.io/#reference/search/searchkeyword/get
+func (tmdb *TMDb) SearchKeyword(name string, options map[string]string) (*KeywordSearchResults, error) {
+	var availableOptions = map[string]struct{}{
+		"page": {}}
+	var keywords KeywordSearchResults
+	safeName := url.QueryEscape(name)
+	optionsString := getOptionsString(options, availableOptions)
+	uri := fmt.Sprintf("%s/search/keyword?query=%s&api_key=%s%s", baseURL, safeName, tmdb.apiKey, optionsString)
+	result, err := callTmdb(uri, &keywords)
+	return result.(*KeywordSearchResults), err
+}
+
+// SearchList searches for lists by name and description
+// http://docs.themoviedb.apiary.io/#reference/search/searchlist/get
+func (tmdb *TMDb) SearchList(name string, options map[string]string) (*ListSearchResults, error) {
+	var availableOptions = map[string]struct{}{
+		"page":          {},
+		"include_adult": {}}
+	var lists ListSearchResults
+	safeName := url.QueryEscape(name)
+	optionsString := getOptionsString(options, availableOptions)
+	uri := fmt.Sprintf("%s/search/list?query=%s&api_key=%s%s", baseURL, safeName, tmdb.apiKey, optionsString)
+	result, err := callTmdb(uri, &lists)
+	return result.(*ListSearchResults), err
 }
 
 // SearchMovie searches for movies by title
@@ -29,4 +202,50 @@ func (tmdb *TMDb) SearchMovie(name string, options map[string]string) (*MovieSea
 	uri := fmt.Sprintf("%s/search/movie?query=%s&api_key=%s%s", baseURL, safeName, tmdb.apiKey, optionsString)
 	result, err := callTmdb(uri, &movies)
 	return result.(*MovieSearchResults), err
+}
+
+// SearchMulti searches the movie, tv show and person collections with a single query
+// http://docs.themoviedb.apiary.io/#reference/search/searchmulti/get
+func (tmdb *TMDb) SearchMulti(name string, options map[string]string) (*MultiSearchResults, error) {
+	var availableOptions = map[string]struct{}{
+		"page":          {},
+		"language":      {},
+		"include_adult": {}}
+	var multis MultiSearchResults
+	safeName := url.QueryEscape(name)
+	optionsString := getOptionsString(options, availableOptions)
+	uri := fmt.Sprintf("%s/search/multi?query=%s&api_key=%s%s", baseURL, safeName, tmdb.apiKey, optionsString)
+	result, err := callTmdb(uri, &multis)
+	return result.(*MultiSearchResults), err
+}
+
+// SearchPerson searches for people by name
+// http://docs.themoviedb.apiary.io/#reference/search/searchperson/get
+func (tmdb *TMDb) SearchPerson(name string, options map[string]string) (*PersonSearchResults, error) {
+	var availableOptions = map[string]struct{}{
+		"page":          {},
+		"search_type":   {},
+		"include_adult": {}}
+	var people PersonSearchResults
+	safeName := url.QueryEscape(name)
+	optionsString := getOptionsString(options, availableOptions)
+	uri := fmt.Sprintf("%s/search/person?query=%s&api_key=%s%s", baseURL, safeName, tmdb.apiKey, optionsString)
+	result, err := callTmdb(uri, &people)
+	return result.(*PersonSearchResults), err
+}
+
+// SearchTv searches for TV shows by title
+// http://docs.themoviedb.apiary.io/#reference/search/searchtv/get
+func (tmdb *TMDb) SearchTv(name string, options map[string]string) (*TvSearchResults, error) {
+	var availableOptions = map[string]struct{}{
+		"page":                {},
+		"language":            {},
+		"search_type":         {},
+		"first_air_date_year": {}}
+	var shows TvSearchResults
+	safeName := url.QueryEscape(name)
+	optionsString := getOptionsString(options, availableOptions)
+	uri := fmt.Sprintf("%s/search/tv?query=%s&api_key=%s%s", baseURL, safeName, tmdb.apiKey, optionsString)
+	result, err := callTmdb(uri, &shows)
+	return result.(*TvSearchResults), err
 }
--- search_test.go
+++ search_test.go
@@ -4,9 +4,49 @@ import (
 	. "gopkg.in/check.v1"
 )
 
+func (s *TmdbSuite) TestSearchCollection(c *C) {
+	result, err := s.tmdb.SearchCollection("Avengers", nil)
+	s.baseTest(&result, err, c)
+	c.Assert(result.Page, Equals, 1)
+	c.Assert(result.Results, Not(HasLen), 0)
+	c.Assert(result.TotalResults, Not(Equals), 0)
+	c.Assert(result.Results[0].ID, Equals, 86311)
+	c.Assert(result.Results[0].Name, Equals, "The Avengers Collection")
+}
+
+func (s *TmdbSuite) TestSearchCompany(c *C) {
+	result, err := s.tmdb.SearchCompany("Lucas", nil)
+	s.baseTest(&result, err, c)
+	c.Assert(result.Page, Equals, 1)
+	c.Assert(result.Results, Not(HasLen), 0)
+	c.Assert(result.TotalResults, Not(Equals), 0)
+	c.Assert(result.Results[0].Name, Equals, "Lucas Entertainment")
+}
+
+func (s *TmdbSuite) TestSearchKeyword(c *C) {
+	result, err := s.tmdb.SearchKeyword("action", nil)
+	s.baseTest(&result, err, c)
+	c.Assert(result.Page, Equals, 1)
+	c.Assert(result.Results, Not(HasLen), 0)
+	c.Assert(result.TotalResults, Not(Equals), 0)
+	c.Assert(result.Results[0].ID, Equals, 207600)
+	c.Assert(result.Results[0].Name, Equals, "action")
+}
+
+func (s *TmdbSuite) TestSearchList(c *C) {
+	result, err := s.tmdb.SearchList("best picture", nil)
+	s.baseTest(&result, err, c)
+	c.Assert(result.Page, Equals, 1)
+	c.Assert(result.Results, Not(HasLen), 0)
+	c.Assert(result.TotalResults, Not(Equals), 0)
+	c.Assert(result.Results[0].ID, Equals, "509ec17b19c2950a0600050d")
+	c.Assert(result.Results[0].Name, Equals, "Best Picture Winners - The Academy Awards")
+}
+
 func (s *TmdbSuite) TestSearchMovie(c *C) {
 	result, err := s.tmdb.SearchMovie("Fight Club", nil)
 	s.baseTest(&result, err, c)
+	c.Assert(result.Page, Equals, 1)
 	c.Assert(result.Results, Not(HasLen), 0)
 	c.Assert(result.TotalResults, Not(Equals), 0)
 	c.Assert(result.Results[0].Title, Equals, "Fight Club")
@@ -15,8 +55,75 @@ func (s *TmdbSuite) TestSearchMovie(c *C) {
 	options["language"] = "es"
 	spanishResult, err := s.tmdb.SearchMovie("Fight Club", options)
 	s.baseTest(&spanishResult, err, c)
+	c.Assert(spanishResult.Page, Equals, 1)
 	c.Assert(spanishResult.Results, Not(HasLen), 0)
 	c.Assert(spanishResult.TotalResults, Not(Equals), 0)
 	c.Assert(spanishResult.Results[0].OriginalTitle, Equals, "Fight Club")
 	c.Assert(spanishResult.Results[0].Title, Equals, "El club de la lucha")
+}
+
+func (s *TmdbSuite) TestSearchMulti(c *C) {
+	result, err := s.tmdb.SearchMulti("American", nil)
+	s.baseTest(&result, err, c)
+	c.Assert(result.Page, Equals, 1)
+	c.Assert(result.Results, Not(HasLen), 0)
+	c.Assert(result.TotalPages > 50, Equals, true)
+	c.Assert(result.TotalResults > 1100, Equals, true)
+	c.Assert(result.Results[1].ID, Equals, 1433)
+	c.Assert(result.Results[1].Name, Equals, "American Dad!")
+
+	var options = make(map[string]string)
+	options["page"] = "5"
+	page5Result, err := s.tmdb.SearchMulti("American", options)
+	s.baseTest(&page5Result, err, c)
+	c.Assert(page5Result.Page, Equals, 5)
+	c.Assert(page5Result.Results, Not(HasLen), 0)
+	c.Assert(page5Result.TotalPages, Equals, result.TotalPages)
+	c.Assert(page5Result.TotalResults, Equals, result.TotalResults)
+	c.Assert(page5Result.Results[0].ID, Equals, 6582)
+	c.Assert(page5Result.Results[0].Name, Equals, "American Gladiators")
+}
+
+func (s *TmdbSuite) TestSearchPerson(c *C) {
+	result, err := s.tmdb.SearchPerson("Brad Pitt", nil)
+	s.baseTest(&result, err, c)
+	c.Assert(result.Page, Equals, 1)
+	c.Assert(result.Results, Not(HasLen), 0)
+	c.Assert(result.TotalPages, Equals, 1)
+	c.Assert(result.TotalResults, Equals, 1)
+	c.Assert(result.Results[0].ID, Equals, 287)
+	c.Assert(result.Results[0].Name, Equals, "Brad Pitt")
+
+	var options = make(map[string]string)
+	options["search_type"] = "ngram"
+	ngramResult, err := s.tmdb.SearchPerson("Brad P", options)
+	s.baseTest(&ngramResult, err, c)
+	c.Assert(ngramResult.Page, Equals, 1)
+	c.Assert(ngramResult.Results, Not(HasLen), 0)
+	c.Assert(ngramResult.TotalPages > result.TotalPages, Equals, true)
+	c.Assert(ngramResult.TotalResults > result.TotalResults, Equals, true)
+	c.Assert(ngramResult.Results[0].ID, Equals, 287)
+	c.Assert(ngramResult.Results[0].Name, Equals, "Brad Pitt")
+}
+
+func (s *TmdbSuite) TestSearchTv(c *C) {
+	result, err := s.tmdb.SearchTv("Breaking Bad", nil)
+	s.baseTest(&result, err, c)
+	c.Assert(result.Page, Equals, 1)
+	c.Assert(result.Results, Not(HasLen), 0)
+	c.Assert(result.TotalPages, Equals, 1)
+	c.Assert(result.TotalResults, Equals, 1)
+	c.Assert(result.Results[0].ID, Equals, 1396)
+	c.Assert(result.Results[0].Name, Equals, "Breaking Bad")
+
+	var options = make(map[string]string)
+	options["search_type"] = "ngram"
+	ngramResult, err := s.tmdb.SearchTv("Breaki", options)
+	s.baseTest(&ngramResult, err, c)
+	c.Assert(ngramResult.Page, Equals, 1)
+	c.Assert(ngramResult.Results, Not(HasLen), 0)
+	c.Assert(ngramResult.TotalPages > result.TotalPages, Equals, true)
+	c.Assert(ngramResult.TotalResults > result.TotalResults, Equals, true)
+	c.Assert(ngramResult.Results[0].ID, Equals, 1396)
+	c.Assert(ngramResult.Results[0].Name, Equals, "Breaking Bad")
 }
