666db03381686a92fde9008d79b19af9f193c783
Ryan Brady <ryanbradynd05@gmail.com>
2015-02-06 00:41:33 -0500

    Increase coverage to 100%

--- main.go
+++ main.go
@@ -21,23 +21,19 @@ func Init(apiKey string) *TMDb {
 
 // ToJSON converts from struct to JSON
 func ToJSON(payload interface{}) (string, error) {
+	jsonRes := []byte("{}") //Default value in case of error
 	jsonRes, err := json.MarshalIndent(payload, "", "  ")
-	if err != nil {
-		return "{}", err
-	}
-	return string(jsonRes), nil
+	return string(jsonRes), err
 }
 
 func getTmdb(url string, payload interface{}) (interface{}, error) {
 	res, err := http.Get(url)
-	if err != nil {
-		return payload, err
+	if err == nil {
+		body, err := ioutil.ReadAll(res.Body)
+		if err == nil {
+			json.Unmarshal(body, &payload)
+		}
 	}
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return payload, err
-	}
-	json.Unmarshal(body, &payload)
 	return payload, err
 }
 
