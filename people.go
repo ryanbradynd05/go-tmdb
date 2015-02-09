1d4bcb742d9d0433bebd0da7e046f75bfa09ccda
Ryan Brady <ryanbradynd05@gmail.com>
2015-02-09 03:34:46 -0500

    Finish people module

--- people.go
+++ people.go
@@ -6,22 +6,40 @@ import (
 
 // Person struct
 type Person struct {
-	ID           int
-	Name         string
-	Overview     string
-	Adult        bool
-	Biography    string
-	Birthday     string
-	Deathday     string
-	Homepage     string
-	AlsoKnownAs  []string       `json:"also_known_as"`
-	PlaceOfBirth string         `json:"place_of_birth"`
-	ProfilePath  string         `json:"profile_path"`
-	Credits      *PersonCredits `json:",omitempty"`
+	ID              int
+	Name            string
+	Overview        string
+	Adult           bool
+	Biography       string
+	Birthday        string
+	Deathday        string
+	Homepage        string
+	AlsoKnownAs     []string               `json:"also_known_as"`
+	PlaceOfBirth    string                 `json:"place_of_birth"`
+	ProfilePath     string                 `json:"profile_path"`
+	Changes         *PersonChanges         `json:",omitempty"`
+	MovieCredits    *PersonMovieCredits    `json:"movie_credits,omitempty"`
+	TvCredits       *PersonTvCredits       `json:"tv_credits,omitempty"`
+	CombinedCredits *PersonCombinedCredits `json:"combined_credits,omitempty"`
+	ExternalIds     *TvExternalIds         `json:"external_ids,omitempty"`
+	Images          *PersonImages          `json:",omitempty"`
+	TaggedImages    *PersonTaggedImages    `json:"tagged_images,omitempty"`
 }
 
-// PersonCredits struct
-type PersonCredits struct {
+// PersonChanges struct
+type PersonChanges struct {
+	Changes []struct {
+		Key   string
+		Items []struct {
+			ID     string
+			Action string
+			Time   string
+		}
+	}
+}
+
+// PersonCombinedCredits struct
+type PersonCombinedCredits struct {
 	ID   int
 	Cast []struct {
 		Adult         bool
@@ -32,6 +50,157 @@ type PersonCredits struct {
 		PosterPath    string `json:"poster_path"`
 		ReleaseDate   string `json:"release_date"`
 		Title         string
+		MediaType     string `json:"media_type"`
+	}
+	Crew []struct {
+		Adult         bool
+		CreditID      string `json:"credit_id"`
+		Department    string
+		ID            int
+		Job           string
+		OriginalTitle string `json:"original_title"`
+		PosterPath    string `json:"poster_path"`
+		ReleaseDate   string `json:"release_date"`
+		Title         string
+		MediaType     string `json:"media_type"`
+	}
+}
+
+// PersonImages struct
+type PersonImages struct {
+	ID       int
+	Profiles []struct {
+		AspectRatio float32 `json:"aspect_ratio"`
+		FilePath    string  `json:"file_path"`
+		ID          string
+		Width       int
+		Height      int
+		Iso639_1    string  `json:"iso_639_1"`
+		VoteAverage float32 `json:"vote_average"`
+		VoteCount   int     `json:"vote_count"`
+	}
+}
+
+// PersonLatest struct
+type PersonLatest struct {
+	ID           int
+	Name         string
+	Adult        bool
+	Biography    string
+	Birthday     string
+	Deathday     string
+	Homepage     string
+	AlsoKnownAs  []string `json:"also_known_as"`
+	PlaceOfBirth string   `json:"place_of_birth"`
+	ProfilePath  string   `json:"profile_path"`
+}
+
+// PersonMovieCredits struct
+type PersonMovieCredits struct {
+	ID   int
+	Cast []struct {
+		Adult         bool
+		Character     string
+		CreditID      string `json:"credit_id"`
+		ID            int
+		OriginalTitle string `json:"original_title"`
+		PosterPath    string `json:"poster_path"`
+		ReleaseDate   string `json:"release_date"`
+		Title         string
+	}
+	Crew []struct {
+		Adult         bool
+		CreditID      string `json:"credit_id"`
+		Department    string
+		ID            int
+		Job           string
+		OriginalTitle string `json:"original_title"`
+		PosterPath    string `json:"poster_path"`
+		ReleaseDate   string `json:"release_date"`
+		Title         string
+	}
+}
+
+// PersonPopular struct
+type PersonPopular struct {
+	Page         int
+	TotalPages   int `json:"total_pages"`
+	TotalResults int `json:"total_results"`
+	Results      []struct {
+		Adult       bool
+		ID          int
+		Name        string
+		Popularity  float32
+		ProfilePath string `json:"profile_path"`
+		KnownFor    []struct {
+			Adult         bool
+			BackdropPath  string `json:"backdrop_path"`
+			ID            int
+			OriginalTitle string `json:"original_title"`
+			ReleaseDate   string `json:"release_date"`
+			PosterPath    string `json:"poster_path"`
+			Popularity    float32
+			Title         string
+			Video         bool
+			VoteAverage   float32 `json:"vote_average"`
+			VoteCount     int     `json:"vote_count"`
+			MediaType     string  `json:"media_type"`
+		} `json:"known_for"`
+	}
+}
+
+// PersonTaggedImages struct
+type PersonTaggedImages struct {
+	ID           int
+	Page         int
+	TotalPages   int `json:"total_pages"`
+	TotalResults int `json:"total_results"`
+	Results      []struct {
+		AspectRatio float32 `json:"aspect_ratio"`
+		FilePath    string  `json:"file_path"`
+		ID          string
+		Width       int
+		Height      int
+		Iso639_1    string  `json:"iso_639_1"`
+		VoteAverage float32 `json:"vote_average"`
+		VoteCount   int     `json:"vote_count"`
+		ImageType   string  `json:"image_type"`
+		MediaType   string  `json:"media_type"`
+		Media       struct {
+			Adult         bool
+			BackdropPath  string `json:"backdrop_path"`
+			ID            int
+			OriginalTitle string `json:"original_title"`
+			PosterPath    string `json:"poster_path"`
+			ReleaseDate   string `json:"release_date"`
+			Title         string
+			Popularity    float32
+		}
+	}
+}
+
+// PersonTvCredits struct
+type PersonTvCredits struct {
+	ID   int
+	Cast []struct {
+		Character    string
+		CreditID     string `json:"credit_id"`
+		EpisodeCount int    `json:"episode_count"`
+		FirstAirDate string `json:"first_air_date"`
+		ID           int
+		Name         string
+		OriginalName string `json:"original_name"`
+		PosterPath   string `json:"poster_path"`
+	}
+	Crew []struct {
+		CreditID     string `json:"credit_id"`
+		Department   string
+		FirstAirDate string `json:"first_air_date"`
+		ID           int
+		Job          string
+		Name         string
+		OriginalName string `json:"original_name"`
+		PosterPath   string `json:"poster_path"`
 	}
 }
 
@@ -47,17 +216,106 @@ func (tmdb *TMDb) GetPersonInfo(id int, options map[string]string) (*Person, err
 	return result.(*Person), err
 }
 
-// GetPersonCredits for a specific person id
-// http://docs.themoviedb.apiary.io/#reference/people/personidmoviecredits/get
-func (tmdb *TMDb) GetPersonCredits(id int, options map[string]string) (*PersonCredits, error) {
+// GetPersonChanges for a specific person id
+// http://docs.themoviedb.apiary.io/#reference/people/personidchanges/get
+func (tmdb *TMDb) GetPersonChanges(id int, options map[string]string) (*PersonChanges, error) {
+	var availableOptions = map[string]struct{}{
+		"start_date": {},
+		"end_date":   {}}
+	var changes PersonChanges
+	optionsString := getOptionsString(options, availableOptions)
+	uri := fmt.Sprintf("%s/person/%v/changes?api_key=%s%s", baseURL, id, tmdb.apiKey, optionsString)
+	result, err := getTmdb(uri, &changes)
+	return result.(*PersonChanges), err
+}
+
+// GetPersonCombinedCredits gets the combined (movie and TV) credits for a specific person id
+// http://docs.themoviedb.apiary.io/#reference/people/personidcombinedcredits/get
+func (tmdb *TMDb) GetPersonCombinedCredits(id int, options map[string]string) (*PersonCombinedCredits, error) {
 	var availableOptions = map[string]struct{}{
 		"language":           {},
 		"append_to_response": {}}
-	var credits PersonCredits
+	var credits PersonCombinedCredits
 	optionsString := getOptionsString(options, availableOptions)
-	uri := fmt.Sprintf("%s/person/%v/credits?api_key=%s%s", baseURL, id, tmdb.apiKey, optionsString)
+	uri := fmt.Sprintf("%s/person/%v/combined_credits?api_key=%s%s", baseURL, id, tmdb.apiKey, optionsString)
 	result, err := getTmdb(uri, &credits)
-	json, _ := ToJSON(result)
-	fmt.Printf("%v\n%v\n", uri, json)
-	return result.(*PersonCredits), err
+	return result.(*PersonCombinedCredits), err
+}
+
+// GetPersonExternalIds gets the external ids for a specific person id
+// http://docs.themoviedb.apiary.io/#reference/people/personidexternalids/get
+func (tmdb *TMDb) GetPersonExternalIds(id int) (*TvExternalIds, error) {
+	var ids TvExternalIds
+	uri := fmt.Sprintf("%s/person/%v/external_ids?api_key=%s", baseURL, id, tmdb.apiKey)
+	result, err := getTmdb(uri, &ids)
+	return result.(*TvExternalIds), err
+}
+
+// GetPersonImages gets the images for a specific person id
+// http://docs.themoviedb.apiary.io/#reference/people/personidimages/get
+func (tmdb *TMDb) GetPersonImages(id int) (*PersonImages, error) {
+	var images PersonImages
+	uri := fmt.Sprintf("%s/person/%v/images?api_key=%s", baseURL, id, tmdb.apiKey)
+	result, err := getTmdb(uri, &images)
+	return result.(*PersonImages), err
+}
+
+// GetPersonLatest gets the latest person id
+// http://docs.themoviedb.apiary.io/#reference/people/personlatest/get
+func (tmdb *TMDb) GetPersonLatest() (*PersonLatest, error) {
+	var latest PersonLatest
+	uri := fmt.Sprintf("%s/person/latest?api_key=%s", baseURL, tmdb.apiKey)
+	result, err := getTmdb(uri, &latest)
+	return result.(*PersonLatest), err
+}
+
+// GetPersonMovieCredits gets the movie credits for a specific person id
+// http://docs.themoviedb.apiary.io/#reference/people/personidmoviecredits/get
+func (tmdb *TMDb) GetPersonMovieCredits(id int, options map[string]string) (*PersonMovieCredits, error) {
+	var availableOptions = map[string]struct{}{
+		"language":           {},
+		"append_to_response": {}}
+	var credits PersonMovieCredits
+	optionsString := getOptionsString(options, availableOptions)
+	uri := fmt.Sprintf("%s/person/%v/movie_credits?api_key=%s%s", baseURL, id, tmdb.apiKey, optionsString)
+	result, err := getTmdb(uri, &credits)
+	return result.(*PersonMovieCredits), err
+}
+
+// GetPersonPopular gets the list of popular people on The Movie Database
+// http://docs.themoviedb.apiary.io/#reference/people/personpopular/get
+func (tmdb *TMDb) GetPersonPopular(options map[string]string) (*PersonPopular, error) {
+	var availableOptions = map[string]struct{}{
+		"page": {}}
+	var popular PersonPopular
+	optionsString := getOptionsString(options, availableOptions)
+	uri := fmt.Sprintf("%s/person/popular?api_key=%s%s", baseURL, tmdb.apiKey, optionsString)
+	result, err := getTmdb(uri, &popular)
+	return result.(*PersonPopular), err
+}
+
+// GetPersonTaggedImages gets the images that have been tagged with a specific person id
+// http://docs.themoviedb.apiary.io/#reference/people/personidtaggedimages/get
+func (tmdb *TMDb) GetPersonTaggedImages(id int, options map[string]string) (*PersonTaggedImages, error) {
+	var availableOptions = map[string]struct{}{
+		"language": {},
+		"page":     {}}
+	var images PersonTaggedImages
+	optionsString := getOptionsString(options, availableOptions)
+	uri := fmt.Sprintf("%s/person/%v/tagged_images?api_key=%s%s", baseURL, id, tmdb.apiKey, optionsString)
+	result, err := getTmdb(uri, &images)
+	return result.(*PersonTaggedImages), err
+}
+
+// GetPersonTvCredits gets the TV credits for a specific person id
+// http://docs.themoviedb.apiary.io/#reference/people/personidtvcredits/get
+func (tmdb *TMDb) GetPersonTvCredits(id int, options map[string]string) (*PersonTvCredits, error) {
+	var availableOptions = map[string]struct{}{
+		"language":           {},
+		"append_to_response": {}}
+	var credits PersonTvCredits
+	optionsString := getOptionsString(options, availableOptions)
+	uri := fmt.Sprintf("%s/person/%v/tv_credits?api_key=%s%s", baseURL, id, tmdb.apiKey, optionsString)
+	result, err := getTmdb(uri, &credits)
+	return result.(*PersonTvCredits), err
 }
--- people_test.go
+++ people_test.go
@@ -18,24 +18,110 @@ func (s *TmdbSuite) TestGetPersonInfo(c *C) {
 	c.Assert(result.PlaceOfBirth, Equals, "Shawnee - Oklahoma - USA")
 
 	var options = make(map[string]string)
-	// options["append_to_response"] = "account_states,alternative_titles,changes,credits,images,keywords,similar,translations,videos"
-	options["append_to_response"] = "credits"
+	options["append_to_response"] = "changes,movie_credits,tv_credits,combined_credits,external_ids,images,tagged_images"
 	allResult, err := s.tmdb.GetPersonInfo(bradPittID, options)
 	s.baseTest(&allResult, err, c)
 	c.Assert(allResult.ID, Equals, bradPittID)
 	c.Assert(allResult.Name, Equals, "Brad Pitt")
-	// c.Assert(allResult.Changes, NotNil)
-	c.Assert(allResult.Credits, NotNil)
-	// c.Assert(allResult.Images, NotNil)
-	// c.Assert(allResult.Keywords, NotNil)
-	// c.Assert(allResult.Similar, NotNil)
-	// c.Assert(allResult.Translations, NotNil)
-	// c.Assert(allResult.Videos, NotNil)
+	c.Assert(allResult.Changes, NotNil)
+	c.Assert(allResult.MovieCredits, NotNil)
+	c.Assert(allResult.TvCredits, NotNil)
+	c.Assert(allResult.CombinedCredits, NotNil)
+	c.Assert(allResult.ExternalIds, NotNil)
+	c.Assert(allResult.Images, NotNil)
+	c.Assert(allResult.TaggedImages, NotNil)
 }
 
-func (s *TmdbSuite) TestGetPersonCredits(c *C) {
-	result, err := s.tmdb.GetPersonCredits(bradPittID, nil)
+func (s *TmdbSuite) TestGetPersonChanges(c *C) {
+	result, err := s.tmdb.GetPersonChanges(bradPittID, nil)
+	s.baseTest(&result, err, c)
+	c.Assert(result.Changes, NotNil)
+	allResultLength := len(result.Changes)
+
+	var janOptions = make(map[string]string)
+	janOptions["start_date"] = "2015-01-01"
+	janOptions["end_date"] = "2015-01-14"
+	janResult, err := s.tmdb.GetPersonChanges(bradPittID, janOptions)
+	s.baseTest(&janResult, err, c)
+	c.Assert(janResult.Changes, NotNil)
+	c.Assert(len(janResult.Changes) >= allResultLength, Equals, true)
+}
+
+func (s *TmdbSuite) TestGetPersonMovieCredits(c *C) {
+	result, err := s.tmdb.GetPersonMovieCredits(bradPittID, nil)
 	s.baseTest(&result, err, c)
 	c.Assert(result.ID, Equals, bradPittID)
 	c.Assert(result.Cast, Not(HasLen), 0)
+}
+
+func (s *TmdbSuite) TestGetPersonTvCredits(c *C) {
+	result, err := s.tmdb.GetPersonTvCredits(bradPittID, nil)
+	s.baseTest(&result, err, c)
+	c.Assert(result.ID, Equals, bradPittID)
+	c.Assert(result.Cast, Not(HasLen), 0)
+}
+
+func (s *TmdbSuite) TestGetPersonCombinedCredits(c *C) {
+	result, err := s.tmdb.GetPersonCombinedCredits(bradPittID, nil)
+	s.baseTest(&result, err, c)
+	c.Assert(result.ID, Equals, bradPittID)
+	c.Assert(result.Cast, Not(HasLen), 0)
+}
+
+func (s *TmdbSuite) TestGetPersonExternalIds(c *C) {
+	result, err := s.tmdb.GetPersonExternalIds(bradPittID)
+	s.baseTest(&result, err, c)
+	c.Assert(result.ID, Equals, bradPittID)
+	c.Assert(result.ImdbID, Equals, "nm0000093")
+	c.Assert(result.TvrageID, Equals, 59436)
+}
+
+func (s *TmdbSuite) TestGetPersonImages(c *C) {
+	result, err := s.tmdb.GetPersonImages(bradPittID)
+	s.baseTest(&result, err, c)
+	c.Assert(result.ID, Equals, bradPittID)
+	c.Assert(len(result.Profiles) > 5, Equals, true)
+}
+
+func (s *TmdbSuite) TestGetPersonTaggedImages(c *C) {
+	result, err := s.tmdb.GetPersonTaggedImages(bradPittID, nil)
+	s.baseTest(&result, err, c)
+	c.Assert(result.ID, Equals, bradPittID)
+	c.Assert(result.Page, Equals, 1)
+	c.Assert(result.Results, HasLen, 20)
+	c.Assert(result.TotalPages >= 3, Equals, true)
+	c.Assert(result.TotalResults >= 45, Equals, true)
+
+	var page2Options = make(map[string]string)
+	page2Options["page"] = "2"
+	page2Result, err := s.tmdb.GetPersonTaggedImages(bradPittID, page2Options)
+	s.baseTest(&page2Result, err, c)
+	c.Assert(page2Result.Page, Equals, 2)
+	c.Assert(page2Result.Results, HasLen, 20)
+	c.Assert(page2Result.TotalPages, Equals, result.TotalPages)
+	c.Assert(page2Result.TotalResults, Equals, result.TotalResults)
+}
+
+func (s *TmdbSuite) TestGetPersonPopular(c *C) {
+	result, err := s.tmdb.GetPersonPopular(nil)
+	s.baseTest(&result, err, c)
+	c.Assert(result.Page, Equals, 1)
+	c.Assert(result.Results, HasLen, 20)
+	c.Assert(result.TotalPages >= 3, Equals, true)
+	c.Assert(result.TotalResults >= 45, Equals, true)
+
+	var page2Options = make(map[string]string)
+	page2Options["page"] = "2"
+	page2Result, err := s.tmdb.GetPersonPopular(page2Options)
+	s.baseTest(&page2Result, err, c)
+	c.Assert(page2Result.Page, Equals, 2)
+	c.Assert(page2Result.Results, HasLen, 20)
+	c.Assert(page2Result.TotalPages, Equals, result.TotalPages)
+	c.Assert(page2Result.TotalResults, Equals, result.TotalResults)
+}
+
+func (s *TmdbSuite) TestGetPersonLatest(c *C) {
+	result, err := s.tmdb.GetPersonLatest()
+	s.baseTest(&result, err, c)
+	c.Assert(result.ID, NotNil)
 }
