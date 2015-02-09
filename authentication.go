3c080d22cbd259d08c6c72533a1848d0b9f700a0
Ryan Brady <ryanbradynd05@gmail.com>
2015-02-09 05:00:34 -0500

    Finish authentication module

--- authentication.go
+++ authentication.go
@@ -8,14 +8,54 @@ import (
 type AuthenticationToken struct {
 	Success      bool
 	RequestToken string `json:"request_token"`
-	ExpiresAt    string `json:"expires_at"`
+	ExpiresAt    string `json:"expires_at,omitempty"`
 }
 
-// GetNewToken generates a valid request token for user based authentication
+// AuthenticationSession struct
+type AuthenticationSession struct {
+	Success   bool
+	SessionID string `json:"session_id"`
+}
+
+// AuthenticationGuestSession struct
+type AuthenticationGuestSession struct {
+	Success        bool
+	GuestSessionID string `json:"guest_session_id"`
+	ExpiresAt      string `json:"expires_at"`
+}
+
+// GetAuthToken generates a valid request token for user based authentication
 // http://docs.themoviedb.apiary.io/#reference/authentication/authenticationtokennew/get
-func (tmdb *TMDb) GetNewToken() (*AuthenticationToken, error) {
+func (tmdb *TMDb) GetAuthToken() (*AuthenticationToken, error) {
 	var token AuthenticationToken
 	uri := fmt.Sprintf("%s/authentication/token/new?api_key=%s", baseURL, tmdb.apiKey)
 	result, err := getTmdb(uri, &token)
 	return result.(*AuthenticationToken), err
+}
+
+// GetAuthValidateToken authenticates a user with a TMDb username and password
+// http://docs.themoviedb.apiary.io/#reference/authentication/authenticationtokenvalidatewithlogin/get
+func (tmdb *TMDb) GetAuthValidateToken(token, user, password string) (*AuthenticationToken, error) {
+	var validToken AuthenticationToken
+	uri := fmt.Sprintf("%s/authentication/token/validate_with_login?api_key=%s&request_token=%s&username=%s&password=%s", baseURL, tmdb.apiKey, token, user, password)
+	result, err := getTmdb(uri, &validToken)
+	return result.(*AuthenticationToken), err
+}
+
+// GetAuthSession generates a session id for user based authentication
+// http://docs.themoviedb.apiary.io/#reference/authentication/authenticationsessionnew/get
+func (tmdb *TMDb) GetAuthSession(token string) (*AuthenticationSession, error) {
+	var session AuthenticationSession
+	uri := fmt.Sprintf("%s/authentication/session/new?api_key=%s&request_token=%s", baseURL, tmdb.apiKey, token)
+	result, err := getTmdb(uri, &session)
+	return result.(*AuthenticationSession), err
+}
+
+// GetAuthGuestSession generates a valid request token for user based authentication
+// http://docs.themoviedb.apiary.io/#reference/authentication/authenticationguestsessionnew/get
+func (tmdb *TMDb) GetAuthGuestSession() (*AuthenticationGuestSession, error) {
+	var session AuthenticationGuestSession
+	uri := fmt.Sprintf("%s/authentication/guest_session/new?api_key=%s", baseURL, tmdb.apiKey)
+	result, err := getTmdb(uri, &session)
+	return result.(*AuthenticationGuestSession), err
 }
--- authentication_test.go
+++ authentication_test.go
@@ -4,9 +4,30 @@ import (
 	. "gopkg.in/check.v1"
 )
 
-func (s *TmdbSuite) TestGetNewToken(c *C) {
-	result, err := s.tmdb.GetNewToken()
+const (
+	userToken    string = ""
+	user         string = ""
+	pw           string = ""
+	session      string = ""
+	guestSession string = ""
+)
+
+func (s *TmdbSuite) TestGetAuthToken(c *C) {
+	result, err := s.tmdb.GetAuthToken()
 	s.baseTest(&result, err, c)
-	c.Assert(result.Success, Equals, true)
-	c.Assert(result.RequestToken, HasLen, 40)
+}
+
+func (s *TmdbSuite) TestGetAuthValidateToken(c *C) {
+	result, err := s.tmdb.GetAuthValidateToken(userToken, user, pw)
+	s.baseTest(&result, err, c)
+}
+
+func (s *TmdbSuite) TestGetAuthSession(c *C) {
+	result, err := s.tmdb.GetAuthSession(userToken)
+	s.baseTest(&result, err, c)
+}
+
+func (s *TmdbSuite) TestGetAuthGuestSession(c *C) {
+	result, err := s.tmdb.GetAuthGuestSession()
+	s.baseTest(&result, err, c)
 }
