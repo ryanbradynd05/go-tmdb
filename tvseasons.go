b48e52fcca8d1a7a8daaa419afe7163724ca2172
Ryan Brady <ryanbradynd05@gmail.com>
2015-02-08 19:15:46 -0500

    Finish tvseason module

--- tvseasons.go
+++ tvseasons.go
@@ -15,6 +15,12 @@ type TvSeason struct {
 	Episodes     []TvEpisode
 }
 
+// TvSeasonImages struct
+type TvSeasonImages struct {
+	ID      int
+	Posters []TvImage
+}
+
 // GetTvSeasonInfo the primary information about a TV season by its season number
 // http://docs.themoviedb.apiary.io/#reference/tv-seasons/tvidseasonseasonnumber/get
 func (tmdb *TMDb) GetTvSeasonInfo(showID, seasonID int, options map[string]string) (*TvSeason, error) {
@@ -26,4 +32,63 @@ func (tmdb *TMDb) GetTvSeasonInfo(showID, seasonID int, options map[string]strin
 	uri := fmt.Sprintf("%s/tv/%v/season/%v?api_key=%s%s", baseURL, showID, seasonID, tmdb.apiKey, optionsString)
 	result, err := getTmdb(uri, &season)
 	return result.(*TvSeason), err
+}
+
+// GetTvSeasonChanges gets a TV season's changes by season ID
+// http://docs.themoviedb.apiary.io/#reference/tv-seasons/tvseasonidchanges/get
+func (tmdb *TMDb) GetTvSeasonChanges(id int, options map[string]string) (*TvChanges, error) {
+	var availableOptions = map[string]struct{}{
+		"start_date": {},
+		"end_date":   {}}
+	var changes TvChanges
+	optionsString := getOptionsString(options, availableOptions)
+	uri := fmt.Sprintf("%s/tv/season/%v/changes?api_key=%s%s", baseURL, id, tmdb.apiKey, optionsString)
+	result, err := getTmdb(uri, &changes)
+	return result.(*TvChanges), err
+}
+
+// GetTvSeasonCredits gets the cast & crew credits for a TV season by season number
+// http://docs.themoviedb.apiary.io/#reference/tv-seasons/tvidseasonseasonnumbercredits/get
+func (tmdb *TMDb) GetTvSeasonCredits(showID, seasonNum int) (*TvCredits, error) {
+	var credits TvCredits
+	uri := fmt.Sprintf("%s/tv/%v/season/%v/credits?api_key=%s", baseURL, showID, seasonNum, tmdb.apiKey)
+	result, err := getTmdb(uri, &credits)
+	return result.(*TvCredits), err
+}
+
+// GetTvSeasonExternalIds gets the external ids for a TV season by season number
+// http://docs.themoviedb.apiary.io/#reference/tv-seasons/tvidseasonseasonnumberexternalids/get
+func (tmdb *TMDb) GetTvSeasonExternalIds(showID, seasonNum int, options map[string]string) (*TvExternalIds, error) {
+	var availableOptions = map[string]struct{}{
+		"language": {}}
+	var ids TvExternalIds
+	optionsString := getOptionsString(options, availableOptions)
+	uri := fmt.Sprintf("%s/tv/%v/season/%v/external_ids?api_key=%s%s", baseURL, showID, seasonNum, tmdb.apiKey, optionsString)
+	result, err := getTmdb(uri, &ids)
+	return result.(*TvExternalIds), err
+}
+
+// GetTvSeasonImages gets the images (posters) that we have stored for a TV season by season number
+// http://docs.themoviedb.apiary.io/#reference/tv-seasons/tvidseasonseasonnumberimages/get
+func (tmdb *TMDb) GetTvSeasonImages(showID, seasonNum int, options map[string]string) (*TvSeasonImages, error) {
+	var availableOptions = map[string]struct{}{
+		"language":               {},
+		"include_image_language": {}}
+	var images TvSeasonImages
+	optionsString := getOptionsString(options, availableOptions)
+	uri := fmt.Sprintf("%s/tv/%v/season/%v/images?api_key=%s%s", baseURL, showID, seasonNum, tmdb.apiKey, optionsString)
+	result, err := getTmdb(uri, &images)
+	return result.(*TvSeasonImages), err
+}
+
+// GetTvSeasonVideos gets the videos that have been added to a TV season
+// http://docs.themoviedb.apiary.io/#reference/tv-seasons/tvidseasonseasonnumbervideos/get
+func (tmdb *TMDb) GetTvSeasonVideos(showID, seasonNum int, options map[string]string) (*TvVideos, error) {
+	var availableOptions = map[string]struct{}{
+		"language": {}}
+	var videos TvVideos
+	optionsString := getOptionsString(options, availableOptions)
+	uri := fmt.Sprintf("%s/tv/%v/season/%v/videos?api_key=%s%s", baseURL, showID, seasonNum, tmdb.apiKey, optionsString)
+	result, err := getTmdb(uri, &videos)
+	return result.(*TvVideos), err
 }
--- tvseasons_test.go
+++ tvseasons_test.go
@@ -4,6 +4,8 @@ import (
 	. "gopkg.in/check.v1"
 )
 
+const gameOfThronesFirstSeasonID int = 3624
+
 func (s *TmdbSuite) TestGetTvSeasonInfo(c *C) {
 	result, err := s.tmdb.GetTvSeasonInfo(gameOfThronesID, 1, nil)
 	s.baseTest(&result, err, c)
@@ -12,4 +14,49 @@ func (s *TmdbSuite) TestGetTvSeasonInfo(c *C) {
 	c.Assert(result.Episodes, HasLen, 10)
 	c.Assert(result.ID, Equals, 3624)
 	c.Assert(result.SeasonNumber, Equals, 1)
+}
+
+func (s *TmdbSuite) TestGetTvSeasonChanges(c *C) {
+	result, err := s.tmdb.GetTvSeasonChanges(gameOfThronesFirstSeasonID, nil)
+	s.baseTest(&result, err, c)
+	c.Assert(result.Changes, NotNil)
+	allResultLength := len(result.Changes)
+
+	var janOptions = make(map[string]string)
+	janOptions["start_date"] = "2015-01-01"
+	janOptions["end_date"] = "2015-01-14"
+	janResult, err := s.tmdb.GetTvSeasonChanges(gameOfThronesFirstSeasonID, janOptions)
+	s.baseTest(&janResult, err, c)
+	c.Assert(janResult.Changes, NotNil)
+	c.Assert(len(janResult.Changes) >= allResultLength, Equals, true)
+}
+
+func (s *TmdbSuite) TestGetTvSeasonCredits(c *C) {
+	result, err := s.tmdb.GetTvSeasonCredits(gameOfThronesID, 1)
+	s.baseTest(&result, err, c)
+	c.Assert(result.ID, Equals, gameOfThronesFirstSeasonID)
+	c.Assert(result.Cast, Not(HasLen), 0)
+	c.Assert(result.Cast[0].Character, Equals, "Eddard Stark")
+	c.Assert(result.Cast[0].Name, Equals, "Sean Bean")
+}
+
+func (s *TmdbSuite) TestGetTvSeasonExternalIds(c *C) {
+	result, err := s.tmdb.GetTvSeasonExternalIds(gameOfThronesID, 1, nil)
+	s.baseTest(&result, err, c)
+	c.Assert(result.ID, Equals, gameOfThronesFirstSeasonID)
+	c.Assert(result.TvdbID, Equals, 364731)
+}
+
+func (s *TmdbSuite) TestGetTvSeasonImages(c *C) {
+	result, err := s.tmdb.GetTvSeasonImages(gameOfThronesID, 1, nil)
+	s.baseTest(&result, err, c)
+	c.Assert(result.ID, Equals, gameOfThronesFirstSeasonID)
+	c.Assert(result.Posters, Not(HasLen), 0)
+}
+
+func (s *TmdbSuite) TestGetTvSeasonVideos(c *C) {
+	result, err := s.tmdb.GetTvSeasonVideos(gameOfThronesID, 1, nil)
+	s.baseTest(&result, err, c)
+	c.Assert(result.ID, Equals, gameOfThronesFirstSeasonID)
+	c.Assert(result.Results, NotNil)
 }
