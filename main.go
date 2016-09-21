e052aaf19567df58b369c42adc5a0380b0cc3a33
Funzi <Funzinator@users.noreply.github.com>
2016-09-21 01:08:00 -0400

    fixed waiting for rateLimitReset wait (#9)
    * fixed waiting for rateLimitReset wait
    found the hint in #7
    
    * made sure we always obey the ratelimit-reset time by adding 1 second to the unix time.

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func getTmdb(url string, payload interface{}) (interface{}, error) {
 	now := time.Now()
 	if rateLimitReset.After(now) { // We have a reset time in the future, so we're out of requests
 		// Wait for rate limiter to be reset
-		<-time.After(now.Sub(rateLimitReset))
+		<-time.After(rateLimitReset.Sub(now))
 	}
 
 	res, err := http.Get(url)
@@ -56,7 +56,7 @@ func getTmdb(url string, payload interface{}) (interface{}, error) {
 		iReset, err := strconv.ParseInt(reset, 10, 64)
 		if err == nil {
 			// Set the reset time here, the next request will trip it
-			rateLimitReset = time.Unix(iReset, 0)
+			rateLimitReset = time.Unix(iReset+1, 0)
 		}
 	}
 
